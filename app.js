977c16f3fb7ede31f214603136763ae07296f562
07240073sherubtse-arch <07240073.sherubtse@rub.edu.bt>
2026-09-20 13:47:40 -0700

    Unit IV Lab 4: Order model, full CRUD, validation, aggregation, and transactions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
+
 dotenv.config();
 
 const app = express();
@@ -10,13 +11,16 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Static files (CSS, client-side JS)
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Parse form data
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 const indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Campus Eats running at http://localhost:${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
+
 const homeController = require('../controllers/homeController');
 const aboutController = require('../controllers/aboutController');
 const menuController = require('../controllers/menuController');
 const orderController = require('../controllers/orderController');
 
-router.post('/orders', orderController.createOrder);
-router.get('/menu', menuController.getMenu);
 router.get('/', homeController.getHome);
+
 router.get('/about', aboutController.getAbout);
 
+router.get('/restaurants/:id/menu', menuController.getMenuByRestaurant);
+
+router.post('/orders', orderController.createOrder);
+router.get('/orders/:id', orderController.getOrder);
+router.post('/orders/:id/update', orderController.updateOrder);
+router.post('/orders/:id/cancel', orderController.cancelOrder);
 
 module.exports = router;
